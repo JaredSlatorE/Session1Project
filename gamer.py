9b157f69aa186bdd3d9f22a88cb91992967eaa6f
JaredSlatorE <jaredbirminghamcu@gmail.com>
2025-09-22 17:16:44 +0100

    Update gamer.py

--- gamer.py
+++ gamer.py
@@ -1,6 +1,5 @@
-print("Hello!")
-print("Im writing this on the epic coding branch!")
-
 def coolFunction():
     gamer = input("Cool function, am i right or am i right?")
     print(gamer)
+
+coolFunction
